c52e1e4a42a04f33b25dec5b2203372f0c9824f0
Shantanu <shantanu@senic.com>
2018-01-23 11:47:25 +0530

    Wifi-tests: BugFix, using field names for terse output.

--- meta-senic/recipes-core/initscripts/initscripts/wifi_test.sh
+++ meta-senic/recipes-core/initscripts/initscripts/wifi_test.sh
@@ -16,13 +16,13 @@ if [ $WIFI_INTERFACES -gt 0 ]; then
     while IFS=':': read -r name interface status ssid;
     do
         echo " Name of interface: $name";
-    done < <(nmcli -t -f common dev | grep 'wifi')
+    done < <(nmcli -t -f type dev | grep 'wifi')
 else
 	echo " wifi interface is missing"	
 fi
 
 # Confirm that we are able to scan available networks
-SCANNED_SSIDS="$(nmcli -t -f common dev wifi list | wc -l)"
+SCANNED_SSIDS="$(nmcli -t -f SSID dev wifi list | wc -l)"
 if [ $SCANNED_SSIDS -gt 0 ]; then
 	echo " Successfully scanned $SCANNED_SSIDS networks"	
 else
@@ -39,9 +39,9 @@ if [ -n "$SSID_NAME" ]; then
             echo " Connecting $name to $SSID_NAME..."
             # nmcli command to connect to network
             nmcli dev wifi con $SSID_NAME password $SSID_PASSWORD ifname $name
-            updated_status=$(nmcli -t -f common dev | grep $name | awk -F':' '{print $3}')
+            updated_status=$(nmcli -t -f DEVICE,TYPE,STATE,CONNECTION dev | grep $name | awk -F':' '{print $3}')
             if [ "$updated_status" == "connected" ]; then
-                connection_name=$(nmcli -t -f common dev | grep $name | awk -F':' '{print $4}')
+                connection_name=$(nmcli -t -f DEVICE,TYPE,STATE,CONNECTION dev | grep $name | awk -F':' '{print $4}')
                 echo " Interface: $name is now connected to $SSID_NAME"
                 # logic to send data using interface
                 ping -q -I $name -c5 google.com > /dev/null
@@ -52,7 +52,7 @@ if [ -n "$SSID_NAME" ]; then
                 fi
                 # nmcli command to close down a connection.
                 nmcli con down id "$connection_name"
-                disconnect_status=$(nmcli -t -f common dev | grep $name | awk -F':' '{print $3}')
+                disconnect_status=$(nmcli -t -f DEVICE,TYPE,STATE,CONNECTION dev | grep $name | awk -F':' '{print $3}')
                 if [ "$disconnect_status" == "disconnected" ]; then
                     echo " Closed connection for: $name"
                 else
@@ -62,7 +62,7 @@ if [ -n "$SSID_NAME" ]; then
                 echo " Failed to connect Interface: $name to $ssid"
             fi
         fi
-    done < <(nmcli -t -f common dev | grep 'wifi')
+    done < <(nmcli -t -f DEVICE,TYPE,STATE,CONNECTION dev | grep 'wifi')
 else
     echo " Please set SSID_NAME and SSID_PASSWORD environment variables"
 fi
