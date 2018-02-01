4e5e34999d8fb4e6a25ad0be1be4c4e337179053
Shantanu <shantanu@senic.com>
2018-02-01 17:49:06 +0530

    Added instructions for usage of hwtests script.

--- meta-senic/recipes-core/initscripts/initscripts/hwtests.sh
+++ meta-senic/recipes-core/initscripts/initscripts/hwtests.sh
@@ -4,6 +4,19 @@
 # Short-Description: Test suite for different HW components
 ### END INIT INFO
 
+if [ "$1" == "--h" ]; then
+    echo "Usage: `basename $0`"
+    echo ""
+    echo "For wifi tests, you need to set and export SSID_NAME and SSID_PASSWORD environment variables."
+    echo "Results are written to results.txt file."
+    echo "Each row has results for a interface identified by first character."
+    echo "w for wifi, b for bluetooth, l for led test."
+    echo "F indicates Failure, P being Pass."
+    echo "Sequence of tests are documented in respective shell scripts."
+    echo ""
+    exit 0
+fi
+
 # Test for LED
 echo 198 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio198/direction
@@ -21,7 +34,7 @@ if [ "$(cat /sys/class/gpio/gpio198/value)" -eq 0 ]; then
     echo " LED switched off."
 else
     echo " Failed to switch off LED."
-    LED_TEST="P"
+    LED_TEST="F"
 fi
 
 echo "l:$LED_TEST" > results.txt
